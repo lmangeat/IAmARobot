f18d345a2470180664ad8cbdbef8560d72b11cad
lmangeat <leon.mangeat@gmail.com>
2015-12-01 13:20:11 +0100

    Ajout route news getAll

---
+++ routes/newsWarframe.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lmangeat on 01/12/2015.
+ */
+var express = require('express');
+var router = express.Router();
+
+var Discord = require("discord.js");
+var mongoose = require('mongoose');
+
+var NewsWarframeDB = require('../models/NewsWarframeDB');
+var NewsWarframe = mongoose.model('NewsWarframe');
+
+router.get('/getAll', function(req, res, next){
+    NewsWarframe.find().exec(function(res, news){
+        res.json(news);
+    });
+
+    res.json(newWarframeToJson());
+});
